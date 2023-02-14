ad681ebf7345baedc7acb3211aeef89e6bdb01e1
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-14 22:38:16 +0300

    add comments

--- week 1/app.js
+++ week 1/app.js
@@ -1,7 +1,9 @@
 const myname = "Maryanne";
 var age = 27;
 var future_job = "Web Developer"
+//concatenation
 var greeting ="My Name is "+ myname +" and I am "+age+" years old I will soon be a "+future_job;
+//interpolation
 var greetings =`My Name is ${myname} and I am ${age} years old I will soon be a ${future_job}`;
 console.log(greeting);
 console.log(greetings);
