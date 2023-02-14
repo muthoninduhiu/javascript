d2069d6111dd80580f95ebda80d0e6b06e953b0c
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-14 22:36:30 +0300

    add comments

---
+++ week 1/app.js
@@ -0,0 +1,8 @@
+const myname = "Maryanne";
+var age = 27;
+var future_job = "Web Developer"
+var greeting ="My Name is "+ myname +" and I am "+age+" years old I will soon be a "+future_job;
+var greetings =`My Name is ${myname} and I am ${age} years old I will soon be a ${future_job}`;
+console.log(greeting);
+console.log(greetings);
+
