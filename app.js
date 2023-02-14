3d5d8b5fff3229c3260febd81ee55ee94bb51f87
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-14 15:32:58 +0300

    add simple website using javascript to make it responsive

---
+++ app.js
@@ -0,0 +1,20 @@
+let min = 10000;
+let max = 90000;
+let num = Math.floor(Math.random() * min) + max;
+
+document.getElementById('ticketNum').innerText = num;
+
+function maybeAlert() {
+    alert('Amazing! :D');
+}
+
+function definetelyPopUp() {
+    alert('See you then!');
+}
+
+function signUpPrompt() {
+    let name = prompt('Great! Please provide your name: ');
+    prompt('Please provide your email: '); // this does nothing. once the user hits enter, their input is gone
+
+    document.getElementById('userMessage').innerText = `See you then, ${name}!`
+}
