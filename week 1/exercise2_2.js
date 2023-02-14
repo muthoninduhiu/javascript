6e25638da911fd0e6169e56d6a6e6dca48e1bb9d
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-15 00:00:44 +0300

    add exercise 2.2

---
+++ week 1/exercise2_2.js
@@ -0,0 +1,9 @@
+/*Exercise 2.2
+Let’s change our minds and remove that last colour from the array. Do this using the
+pop() method.
+*/
+let colored_two = ['red','pink','maroon','amber','blue','orange','black']
+
+let remove = colored_two.pop()
+
+console.log(`Exercise 2.2(POP) :${colored_two}`)
