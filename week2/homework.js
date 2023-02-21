fca6e951cb5999dcb79b154d299896105bf451b8
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-21 22:00:26 +0300

    add week 2 homework

--- week2/homework.js
+++ week2/homework.js
@@ -2,3 +2,26 @@
 We know that while loops can be infinite. Create a do while loop that takes
 in user input and returns different log statement options, but only if the
 input statement is not stop. If the statement is stop, break the loop.*/
+
+let userInput;
+
+do {
+  userInput = prompt("Please enter a statement");
+
+  switch (userInput) {
+    case "hello":
+      console.log("Hello there!");
+      break;
+    case "how are you":
+      console.log("I'm doing well, thanks for asking.");
+      break;
+    case "goodbye":
+      console.log("Goodbye!");
+      break;
+    case "stop":
+      break;
+    default:
+      console.log("I'm sorry, I didn't understand that.");
+      break;
+  }
+} while (userInput !== "stop");
