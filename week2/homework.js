4b93594225b4fd8d3066fffb0da463223e1ac504
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-21 15:39:52 +0300

    ass week 2 homework

---
+++ week2/homework.js
@@ -0,0 +1,4 @@
+/* Homework Task - Quitting the Loop
+We know that while loops can be infinite. Create a do while loop that takes
+in user input and returns different log statement options, but only if the
+input statement is not stop. If the statement is stop, break the loop.*/
