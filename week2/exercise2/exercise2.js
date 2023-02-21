2cbf9f81cdbb748d345ebeb7a18c447cc4c178f6
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-21 21:59:49 +0300

    add week 2 exercise 2

---
+++ week2/exercise2/exercise2.js
@@ -0,0 +1,19 @@
+// Exercise 2.0
+/*Let’s create a function to add two numbers together. They can be
+any two numbers you can think of. To demonstrate this, we’ll start by writing a function
+called addTwoNumbers.*/
+/*Exercise 2.1
+The function should have two parameters, num1 and num2. It should then return these two
+numbers added together.*/
+function addNumbers(a,b){
+    return a + b
+}
+/*Exercise 2.2
+Test this function with the following numbers:
+- 2, 2
+- 9, 3
+- 2938, 2
+The results should be 4, 12, and 2940.*/
+console.log(addNumbers(2,2))
+console.log(addNumbers(9,3))
+console.log(addNumbers(2938s,2))
