3f5c630b106b1262aef5eea292efae14375d6a9c
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-02 14:16:46 +0300

    add week 3 javascript

---
+++ week3/app.js
@@ -0,0 +1,8 @@
+alert("this is mad")
+let font_family = document.getElementById('paragraph').style.fontFamily('sans-serif');
+let font_size = document.getElementById('paragraph').style.fontSize('18px');
+let width = document.getElementById('paragraph').style.width('100px');
+let border = document.getElementById('paragraph').style.border('1px');   
+let borderColor = document.getElementById('paragraph').style.border_color('orange')
+let padding = document.getElementById('paragraph').style.padding('30px');
+   
