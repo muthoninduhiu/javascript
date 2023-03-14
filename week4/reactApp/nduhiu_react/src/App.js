6232279811fc00b3eea4aac061fb21fde57e02dc
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-14 23:28:32 +0300

    add week 4 files

--- week4/reactApp/nduhiu_react/src/App.js
+++ week4/reactApp/nduhiu_react/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+import logo from './pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>I run this bitch!</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HELLO NDUHIU!! LETS PLAY!!
+            {/* Your favourite hobby or activity you
+                like to do (e.g. for downtime)
+              ● Any relevant rules (e.g. how to play if
+                its a sport hobby)
+              ● Why do you do it, why do you enjoy it
+              ● Effectively your hobby 101 website */
+            }
+        <h1>My favorite hobby is walking my dog</h1>
+        <h3>The only rule to walking the little guy is its done everyday lol!</h3>
+        <p>I love doing it because it adds energy to my day and i feel amazing after
+         . Plus its a great way to stay fit!
         </p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
