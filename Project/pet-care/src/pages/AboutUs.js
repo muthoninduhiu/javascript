366831d495535caf51ebee825b4e80a6f8f221a9
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-17 15:03:08 +0300

    add an about us page

---
+++ Project/pet-care/src/pages/AboutUs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our pet care app! We are a team of passionate pet lovers dedicated to providing the best care for your furry friends.</p>
+    </div>
+  );
+}
